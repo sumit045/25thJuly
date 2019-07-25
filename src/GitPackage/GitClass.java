ee4839880a4511372881c9e0c075c4f75843bff7
sumit045 <sumit619.hellboy@gmail.com>
2019-07-25 18:20:01 +0530

    This will open browser and get FB

--- src/GitPackage/GitClass.java
+++ src/GitPackage/GitClass.java
@@ -1,11 +1,17 @@
 package GitPackage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class GitClass {
 
 	public static void main(String[] args) {
 		
 		System.out.println("This is for GIT");
-
+		System.setProperty("webdriver.gecko.driver", "C:\\repo\\GitProject\\Driver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://www.facebook.com");
 	}
 
 }
